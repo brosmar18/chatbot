f5a60a412634a3a3753cfe8fb6ee5c2c03056a7b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 12:29:11 -0500

    greeting logic added

--- src/main.py
+++ src/main.py
@@ -9,3 +9,13 @@ def greet_user():
     current_time = datetime.now().time()
 
     # Determine the greeting based on the time. 
+    if current_time.hour < 12: 
+        greeting = "Good Morning"
+    elif 12 <= current_time.hour < 18:
+        greeting = "Godo afternoon"
+    else: 
+        greeting = "Good Evening"
+
+    print(f"{greeting}, welcome to chatbot!")
+
+    
