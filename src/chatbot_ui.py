ff16c74a34bae5345f5f339da4384e26601c16a6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 12:42:15 -0500

    main window created

--- src/chatbot_ui.py
+++ src/chatbot_ui.py
@@ -1,11 +1,14 @@
-import tkinter as tik
+import tkinter as tk
 from tkinter import ttk
 
 def on_send():
     user_input = user_entry.get()
-    # TODO: Process user input and get chatbot response. 
-    chat_area.insert(tik.End, f"You: {user_input}\n")
-    chat_area.insert(tik.END, f"Chatbot: I'm still learning...\n")
+    # TODO: Process user input and get chatbot response
+    chat_area.insert(tk.END, f"You: {user_input}\n")
+    chat_area.insert(tk.END, f"Chatbot: I'm still learning...\n")
     user_entry.delete(0, tk.END)
 
-    
+# Create the main window
+root = tk.Tk()
+root.title("Chatbot")
+
