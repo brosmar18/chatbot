81a43172a4630ed9799cc07bbe601a66edc7cff5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 12:52:11 -0500

    import added, greet_user() setup for build

--- src/chatbot_ui.py
+++ src/chatbot_ui.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime
+
+def greet_user():
+
+    # Greet user based on current time of the day. 
+    # Get current time.
+    current_time = datetime.now().time()
+
+
 
 def on_send():
     user_input = user_entry.get()
@@ -15,6 +24,9 @@ root.title("Chatbot")
 # Create a text area for chat
 chat_area = tk.Text(root, wrap=tk.WORD, width=50, height=15)
 chat_area.pack(padx=10, pady=10)
+
+# Call greet_user to display greenting when app launches
+greet_user()
 
 # Entry box for user input
 user_entry = ttk.Entry(root, width=50)
