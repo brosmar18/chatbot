391147899f1fdb25d3330e74aa9f0c71fe52a10d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 13:42:20 -0500

    nlu logic added, on_send updated

--- src/chatbot_ui.py
+++ src/chatbot_ui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
-from main import handle_simple_conversation, handle_knowledge_query, analyze_sentiment
+from main import handle_simple_conversation, handle_knowledge_query, analyze_sentiment, handle_advanced_conversation
 
 def greet_user():
 
@@ -21,20 +21,23 @@ def greet_user():
 
 def on_send():
     user_input = user_entry.get()
-    # TODO: Process user input and get chatbot response
     chat_area.insert(tk.END, f"You: {user_input}\n")
     
-    # Handle simple convos and get chatbot's response
+    # Try handling simple conversation first
     chatbot_response = handle_simple_conversation(user_input)
-
-    # If the response is the default "I don't understand", try the knowledge base. 
+    
+    # If the response is the default "I don't understand", try the knowledge base
     if chatbot_response == "I'm sorry, I don't understand that.":
         chatbot_response = handle_knowledge_query(user_input)
-
-    # If teh response is still the default, perform sentiment analysis
+    
+    # If the response is still the default, perform sentiment analysis
     if chatbot_response == "I'm sorry, I don't have that information.":
         chatbot_response = analyze_sentiment(user_input)
 
+    # If no other methods work, use advanced NLU
+    if chatbot_response in ["I'm sorry, I don't understand that.", "I'm sorry, I don't have that information.", "I'm sorry, I couldn't find any specific entities in your message."]:
+        chatbot_response = handle_advanced_conversation(user_input)
+        
     chat_area.insert(tk.END, f"Chatbot: {chatbot_response}\n")
     user_entry.delete(0, tk.END)
 
