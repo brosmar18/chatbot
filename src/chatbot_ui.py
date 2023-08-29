0b89c2d120db8af7f272307a8c4c0752f5d4110a
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 12:37:50 -0500

    userin put and chatbot response added

--- src/chatbot_ui.py
+++ src/chatbot_ui.py
@@ -1,2 +1,11 @@
 import tkinter as tik
 from tkinter import ttk
+
+def on_send():
+    user_input = user_entry.get()
+    # TODO: Process user input and get chatbot response. 
+    chat_area.insert(tik.End, f"You: {user_input}\n")
+    chat_area.insert(tik.END, f"Chatbot: I'm still learning...\n")
+    user_entry.delete(0, tk.END)
+
+    
