4e160f0bd7eec509ce624cc7d98fd8cb4612d83d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-29 13:00:43 -0500

    simple responses added

--- src/main.py
+++ src/main.py
@@ -1,23 +1,6 @@
-from datetime import datetime
-
-def greet_user():
-    """
-        Greet user based on the current time of the day.
-    """
-
-    # Get current time. 
-    current_time = datetime.now().time()
-
-    # Determine the greeting based on the time. 
-    if current_time.hour < 12: 
-        greeting = "Good Morning"
-    elif 12 <= current_time.hour < 18:
-        greeting = "Godo afternoon"
-    else: 
-        greeting = "Good Evening"
-
-    print(f"{greeting}, welcome to chatbot!")
-
-
-# Call the function
-greet_user()
+simple_responses = {
+    "hello": "Hi there!",
+    "how are you": "I'm just a program, so I don't have feelings, but thanks for asking!",
+    "bye": "Goodbye!",
+    "who are you": "I'm a chatbot, here to assist you."
+}
